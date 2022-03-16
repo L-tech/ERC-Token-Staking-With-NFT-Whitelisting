5d1a5906f6693a1e97db0f4c2d175e066ab075ab
L-tech <legendabrahamonoja@gmail.com>
2022-03-16 22:10:17 +0100

    update: stake token scripts

---
+++ scripts/staketoken.js
@@ -0,0 +1,25 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+const hre = require("hardhat");
+const { ethers } = require("hardhat");
+// const BoredApeNFTHolder = "0x4548d498460599286ce29baf9e6b775c19385227";
+// const BoredApeTokenAddress = "0x0ed64d01D0B4B655E410EF1441dD677B695639E7";
+
+async function main() {
+  const stakeContract = await hre.ethers.getContractFactory("BoardStake");
+  const stake = await stakeContract.deploy();
+  await stake.deployed();
+  console.log("Contract Address", stake.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
